ee666631df74b6244143330b06fdbf0a294a5871
Sai pradeep <142535919+PradeepSaiC@users.noreply.github.com>
2026-06-15 11:27:34 +0530

    Update app.get route and listen address

--- backend/index.js
+++ backend/index.js
@@ -62,10 +62,10 @@ app.get("/create-room", (req, res) => {
 
 app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Listening on port ${PORT}`);
 });
